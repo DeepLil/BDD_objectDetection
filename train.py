e850293c17101fe8932584639659327df00775e5
Phani <79618601+DeepLil@users.noreply.github.com>
2026-03-22 23:57:02 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+def train_yolo_model(data_yaml, model_name="yolov8n.pt", epochs=1):
+    # Load the model
+    model = YOLO(model_name)
+    
+    # Train the model
+    results = model.train(data=data_yaml, epochs=epochs, imgsz=640, resume = True, plots=True)
+
+    return results
+
+results = train_yolo_model("D:/Phani_Works/2025/Hailo/BDD/data.yaml", epochs=1)
